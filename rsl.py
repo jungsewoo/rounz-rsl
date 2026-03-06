5056f2e35c87dea6cfaa389a135d0557ddaeb007
jungsewoo <known1509@gmail.com>
2026-03-06 17:44:16 +0900

    Update rsl.py

--- rsl.py
+++ rsl.py
@@ -180,8 +180,7 @@ if st.session_state['active_tab'] == "check":
 # [화면 2] 달성 혜택 안내
 # ==========================================
 elif st.session_state['active_tab'] == "benefit":
-    <div class="naver-logo">N</div>
-    st.markdown("#### **구간별 달성 혜택 상세**")
+     st.markdown("#### **🏆 구간별 달성 혜택 상세**")
     st.write("")
     
     st.markdown(f"""
@@ -229,6 +228,7 @@ elif st.session_state['active_tab'] == "benefit":
             </div>
         </div>
         """, unsafe_allow_html=True)
+
 
 
 
